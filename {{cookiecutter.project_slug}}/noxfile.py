4512fd8a348536ba3e33108f81ad25f056e9edfc
Holger Frey <mail@holgerfrey.de>
2023-08-17 11:30:44 +0200

    added example downstream testing to noxfile

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -1,3 +1,6 @@
+import pathlib
+import tempfile
+
 import nox
 
 
@@ -6,3 +9,22 @@ def tests(session):
     session.install(".[test]")
 
     session.run("pytest", *session.posargs)
+
+
+@nox.session(python=["3.9", "3.10", "3.11"])
+@nox.parametrize(
+    ("repo", "test_dir"),
+    [
+        ("https://git.example.com/user/repo", "tests"),
+    ],
+)
+def downstream(session, repo, test_dir):
+    session.install(".")
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        session.run("git", "clone", repo, tmpdir, external=True)
+
+        session.install(f"{tmpdir}[test]")
+
+        test_path = pathlib.Path(tmpdir) / test_dir
+        session.run("pytest", test_path)
